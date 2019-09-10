2067fef030483ab33194889cae67813178e0e801
Emanuele Mazzante <emanuele78m@gmail.com>
2019-09-10 07:40:06 +0200

    Added artisan comman for scraper

---
+++ app/Console/Commands/StartJavaScraper.php
@@ -0,0 +1,45 @@
+<?php
+	
+	namespace App\Console\Commands;
+	
+	use Illuminate\Console\Command;
+	
+	class StartJavaScraper extends Command {
+		
+		/**
+		 * The name and signature of the console command.
+		 *
+		 * @var string
+		 */
+		protected $signature = 'scraper:start';
+		
+		/**
+		 * The console command description.
+		 *
+		 * @var string
+		 */
+		protected $description = 'Start the java scraper program';
+		
+		/**
+		 * Create a new command instance.
+		 *
+		 * @return void
+		 */
+		public function __construct() {
+			
+			parent::__construct();
+		}
+		
+		/**
+		 * Execute the console command.
+		 *
+		 * @return mixed
+		 */
+		public function handle() {
+			
+			$this->line('Starting the java scraper program');
+			$this->line('java -jar ' . config('app.java_scraper_path') . ' 2>&1');
+			exec('java -jar ' . config('app.java_scraper_path') . ' 2>&1');
+			$this->line('Scraping finished');
+		}
+	}
